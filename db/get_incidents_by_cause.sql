7cb074cb0c5c590399df2d54565cce84e5af28c9
Steven Nagie <sanagie@yahoo.com>
2016-11-16 13:12:01 -0700

    Everything's working, now I might break something.

---
+++ db/get_incidents_by_cause.sql
@@ -0,0 +1,5 @@
+SELECT inc.id, inc.us_state, inj.name AS injury, a.name AS affected_area, c.name AS cause FROM incidents inc
+JOIN injuries inj ON inc.injury_id = inj.id
+JOIN affected_areas a ON inj.affected_area_id = a.id
+JOIN causes c ON inc.cause_id = c.id
+WHERE c.name ILIKE concat('%', $1::TEXT, '%');
--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var massive = require('massive');
+
+var db = massive.connectSync({
+  connectionString : 'postgres://postgres:bbNLtrc99@localhost/massive_demo'
+});
 
 var app = express();
 app.use(bodyParser.json());
+app.set('db', db);
+console.log(db.get_all_injuries);
 
 var port = 3000;
 
 app.get('/incidents', function(req, res) {
-  console.log('GET /incidents');
+  var cause = req.query.cause;
+  console.log(cause);
+
+  if(cause) {
+    db.get_incidents_by_cause([cause], function(err, incidents){
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.json(incidents);
+    });
+  } else {
+    db.get_all_incidents(function(err, incidents){
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.json(incidents);
+    });
+  }
 });
 
 app.post('/incidents', function(req, res) {
+  console.log(req.body);
+  db.post_incidents([req.body.us_state, req.body.injury_id, req.body.cause_id], function(err, incidents) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(incidents);
+    }
+  });
   console.log('POST /incidents');
 });
 
