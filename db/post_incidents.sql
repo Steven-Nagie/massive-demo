345184da470ed1ee79a1c346cb062719aa535ba2
Steven Nagie <sanagie@yahoo.com>
2016-11-16 14:27:05 -0700

    j;lk

--- db/post_incidents.sql
+++ db/post_incidents.sql
@@ -9,4 +9,8 @@ JOIN affected_areas a ON inj.affected_area_id = a.id
 JOIN causes c ON inc.cause_id = c.id
 WHERE inc.id = (SELECT id FROM new_incident);
 
---Problem seems to be that id does not exist when we insert the new row, so it looks for and grabs something that isn't there, which results in sending back an empty array. 
+--Problem seems to be that id does not exist when we insert the new row, so it looks for and grabs something that isn't there, which results in sending back an empty array.
+
+-- INSERT INTO incidents (us_state, injury_id, cause_id)
+--   VALUES ($1, $2, $3)
+--   RETURNING id;
